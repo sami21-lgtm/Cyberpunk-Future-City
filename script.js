e53dc055fa7d6cfed028bbb8a4becb123a7dfde2
Emtiaz Hossain Sami <samiemtiaz38@gmail.com>
2026-05-04 10:30:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('.carousel .list');
     const thumb = document.querySelector('.carousel .thumbnail');
 
-    nextBtn.addEventListener('click', () => {
+    nextBtn.onclick = () => {
         let items = document.querySelectorAll('.carousel .list .item');
         let thumbs = document.querySelectorAll('.carousel .thumbnail .item');
         
         list.appendChild(items[0]);
         thumb.appendChild(thumbs[0]);
-    });
+    };
 
-    prevBtn.addEventListener('click', () => {
+    prevBtn.onclick = () => {
         let items = document.querySelectorAll('.carousel .list .item');
         let thumbs = document.querySelectorAll('.carousel .thumbnail .item');
         
         list.prepend(items[items.length - 1]);
         thumb.prepend(thumbs[thumbs.length - 1]);
-    });
+    };
 });
