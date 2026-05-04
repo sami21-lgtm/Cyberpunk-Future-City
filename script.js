2dbe84802ccc42b68091a68c384abec647addf11
Emtiaz Hossain Sami <samiemtiaz38@gmail.com>
2026-05-04 09:56:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-let nextBtn = document.getElementById('next');
-let prevBtn = document.getElementById('prev');
-let carousel = document.querySelector('.carousel');
-let listItems = document.querySelector('.carousel .list');
-let thumbItems = document.querySelector('.carousel .thumbnail');
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
 
-nextBtn.onclick = () => showSlider('next');
-prevBtn.onclick = () => showSlider('prev');
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 
-function showSlider(type) {
-    let items = document.querySelectorAll('.carousel .list .item');
-    let thumbs = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next') {
-        listItems.appendChild(items[0]);
-        thumbItems.appendChild(thumbs[0]);
-        carousel.classList.add('next');
-    } else {
-        listItems.prepend(items[items.length - 1]);
-        thumbItems.prepend(thumbs[thumbs.length - 1]);
-        carousel.classList.add('prev');
-    }
-
-    setTimeout(() => {
-        carousel.classList.remove('next');
-        carousel.classList.remove('prev');
-    }, 500);
+nextDom.onclick = function(){
+    showSlider('next');    
 }
 
-// Auto-run every 8 seconds
-setInterval(() => { nextBtn.click(); }, 8000);
+prevDom.onclick = function(){
+    showSlider('prev');
+}
+
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    }
+}
